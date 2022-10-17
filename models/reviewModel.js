2876d750cce27ccd0feafcd07abb8c4cef93a455
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-10-17 17:10:26 +0300

    calculate avg ratings on tours1

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,6 +1,7 @@
 //A review needs to belong to a tour and an author. 
 //So we have to implement parent referencing.
 const mongoose = require("mongoose");
+const Tour = require("./tourModel.js");
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -44,6 +45,31 @@ reviewSchema.pre(/^find/, function(next) {
         select: "name photo"
     })
 });
+
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+    const stats = await this.aggregate([{
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: "$tour",
+                nRating: { $sum: 1 },
+                avgRating: { $avg: "$rating" }
+            }
+        }
+    ])
+    console.log(stats)
+
+    await Tour.findByIdAndUpdate(tourId, {
+        ratingsQuantity: stats[0].nRating,
+        ratingsAverage: stats[0].avgRating
+    })
+};
+
+reviewSchema.post("save", function() {
+    //this points to current review
+    this.constructor.calcAverageRatings(this.tour)
+})
 
 const Review = mongoose.model("Review", reviewSchema);
 
